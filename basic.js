73164a5a6b48710d30905961a394bcc1f89e0c40
shahed5358e <mohammadshahed5358@gmail.com>
2022-02-20 12:04:37 +0600

    basic js , dom html , dom js added

---
+++ basic.js
@@ -0,0 +1,44 @@
+// variable
+var deposit = 400;
+// condition
+if (deposit > 500) {
+
+}
+if (deposit < 200) {
+
+}
+if (deposit == 500) {
+
+}
+if (deposit != 500) {
+
+}
+if (deposit >= 500) {
+
+}
+// Array
+const numbers = [23, 32, 4, 5, 6, 87,];
+const numberCount = numbers.length;
+numbers.push(324);
+numbers.pop();
+numbers[2] = 4444;
+// check whether 222 included in the array
+if (numbers.indexOf(222) != -1) {
+
+}
+else if (numbers.includes(222)) {
+
+}
+// loop
+// while
+for (const number of numbers) {
+
+}
+// function
+function fullName(first, second) {
+    const name = first + '' + second;
+}
+const person = fullName('Rahim', 'Ali')
+
+// Object
+const bottle = { color: 'yellow', contain: 'water', price: '20' }
