8fde2ca9c13cb66a7a55b8b6008b7d7f33d45756
shahed5358e <mohammadshahed5358@gmail.com>
2022-02-20 13:15:08 +0600

    template-string add

---
+++ template-string.js
@@ -0,0 +1,26 @@
+const priya = 'asif akbor';
+const meye = 'meye tmi ki dokko cino';
+const kobita = `kobita tumi sopno charini`;
+const multiLine = 'this is my first line. \n' +
+    'this is my second line.\n' +
+    'third line text here.\n' +
+    'fourth line text here.';
+
+const multiLineNew = `this is my first line 
+this is my second line 
+this is my third line 
+this is my fourth line`;
+
+const friends = ['abul', 'babul', 'kavul', 'savul']
+const friendCount = 5;
+const oldHeader = '<h3 > shahedul islam</h3>';
+const newHeader = '<h3 > shahedul islam-' + friendCount + '</h3>';
+const new1 = `<h3 > shahedul islam-${friendCount}</h3>`;
+
+const first = 'Mamun';
+const last = 'chowdory';
+// const fullOld = 'this person name is :' first + ' ' + last;
+fullNew = `This person name is:${first} ${last}. Has Money ${friends.length * 500}. He lives in Dhaka.`;
+console.log(fullNew);
+
+// console.log(multiLineNew);
